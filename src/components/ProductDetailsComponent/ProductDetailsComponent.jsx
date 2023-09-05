7a45b43197c2f398fe2cb471daabf145647da345
XQuang1607 <xuanquang160702@mail.com>
2023-09-05 20:34:49 +0700

    V11

---
+++ src/components/ProductDetailsComponent/ProductDetailsComponent.jsx
@@ -0,0 +1,44 @@
+import { Col, Image, Row } from 'antd'
+import React from 'react'
+import imageProduct from '../../assets/images/test.webp'
+import imageProductSmall from '../../assets/images/imagesmall.webp'
+import { WrapperStyleColImage, WrapperStyleImageSmall } from './style'
+
+const ProductDetailsComponent = () => {
+    const onChange = () => { }
+    return (
+        <Row style={{ padding: '16px', background: '#fff', borderRadius: '4px' }}>
+            <Col span={10} >
+                <Image src={imageProduct} alt="image prodcut" preview={false} />
+                <Row style={{ paddingTop: '10px', justifyContent: 'space-between' }}>
+                    <WrapperStyleColImage span={4}>
+                        <WrapperStyleImageSmall src={imageProductSmall} alt="image small" preview={false} />
+                    </WrapperStyleColImage>
+                    <WrapperStyleColImage span={4}>
+                        <WrapperStyleImageSmall src={imageProductSmall} alt="image small" preview={false} />
+                    </WrapperStyleColImage>
+
+                    <WrapperStyleColImage span={4}>
+                        <WrapperStyleImageSmall src={imageProductSmall} alt="image small" preview={false} />
+                    </WrapperStyleColImage>
+
+                    <WrapperStyleColImage span={4}>
+                        <WrapperStyleImageSmall src={imageProductSmall} alt="image small" preview={false} />
+                    </WrapperStyleColImage>
+
+                    <WrapperStyleColImage span={4}>
+                        <WrapperStyleImageSmall src={imageProductSmall} alt="image small" preview={false} />
+                    </WrapperStyleColImage>
+
+                    <WrapperStyleColImage span={4}>
+                        <WrapperStyleImageSmall src={imageProductSmall} alt="image small" preview={false} />
+                    </WrapperStyleColImage>
+
+                </Row>
+            </Col>
+            <Col span={14} >col-14</Col>
+        </Row>
+    )
+}
+
+export default ProductDetailsComponent
---
+++ src/components/ProductDetailsComponent/style.js
@@ -0,0 +1,14 @@
+import { Col, Image, InputNumber } from "antd";
+import styled from "styled-components";
+
+export const WrapperStyleImageSmall = styled(Image)
+`
+    height: 64px;
+    width: 64px;
+`
+
+export const WrapperStyleColImage = styled(Col)
+`
+    flex-basis: unset;
+    display: flex;
+`
