fbf78a9199d7434d56367f998d14c5ee7beb610f
XQuang1607 <quangnxde160580@fpt.edu.vn>
2024-02-21 21:24:43 +0700

    filter-search-paginated-tableProduct

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 
+export const axiosJWT = axios.create()
+
 export const getAllProduct = async() => {
     const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`)
     return res.data
@@ -7,5 +9,28 @@ export const getAllProduct = async() => {
 
 export const createProduct = async(data) => {
     const res = await axios.post(`${process.env.REACT_APP_API_URL}/product/create`, data)
+    return res.data
+}
+
+export const getDetailsProduct = async(id) => {
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-details/${id}`)
+    return res.data
+}
+
+export const updateProduct = async(id, access_token, data) => {
+    const res = await axiosJWT.put(`${process.env.REACT_APP_API_URL}/product/update/${id}`, data, {
+        headers: {
+            token: `Bearer ${access_token}`,
+        }
+    })
+    return res.data
+}
+
+export const deleteProduct = async(id, access_token) => {
+    const res = await axiosJWT.delete(`${process.env.REACT_APP_API_URL}/product/delete/${id}`, {
+        headers: {
+            token: `Bearer ${access_token}`,
+        }
+    })
     return res.data
 }
