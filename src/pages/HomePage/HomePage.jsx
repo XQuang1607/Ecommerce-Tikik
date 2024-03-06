edb067bbddc672ccb24821b70da1ac05250b7b06
XQuang1607 <quangnxde160580@fpt.edu.vn>
2024-03-06 17:15:11 +0700

    details-product

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -60,6 +60,7 @@ const HomePage = () => {
                   type={product.type}
                   selled={product.selled}
                   discount={product.discount}
+                  id={product._id}
                 />
               )
             })}
